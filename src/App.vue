f4a6d089754f4940cdc3842ab4d8eb379e24bf13
Prappo Prince <prappo.prince@gmail.com>
2021-06-27 15:20:54 +0600

    few update

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,48 @@
 <template>
   <div>
-    <div class="djpp">
-      <div class="video-content" v-html="content"></div>
-      <div class="video-wraper">
-        <div v-show="showPointer" @click="play" :id="pointerID" class="pointer">
-          <div class="hotspot"></div>
-          <div class="hotspot-text" v-html="clickText"></div>
+    <button id="show-modal" @click="showModal = true">Show Modal</button>
+    <transition v-show="showModal" @close="showModal = false" name="modal">
+      <div class="modal-mask">
+        <div class="modal-wrapper">
+          <div class="modal-container">
+            <div class="modal-header">
+              <slot name="header"> default header </slot>
+            </div>
+
+            <div class="modal-body">
+              <slot name="body">
+                <div class="djpp">
+                  <div class="video-content" v-html="content"></div>
+                  <div class="video-wraper">
+                    <div
+                      v-show="showPointer"
+                      @click="play"
+                      :id="pointerID"
+                      class="pointer"
+                    >
+                      <div class="hotspot"></div>
+                      <div class="hotspot-text" v-html="clickText"></div>
+                    </div>
+                    <video :id="playerID" class="player">
+                      <source src="video.mp4" type="video/mp4" />
+                    </video>
+                  </div>
+                </div>
+              </slot>
+            </div>
+
+            <div class="modal-footer">
+              <slot name="footer">
+                default footer
+                <button class="modal-default-button" @click="restart">
+                  OK
+                </button>
+              </slot>
+            </div>
+          </div>
         </div>
-        <video :id="playerID" class="player">
-          <source src="video.mp4" type="video/mp4" />
-        </video>
       </div>
-      
-    </div>
-  
-  <div class="dots">
-    <div v-for="(result , i ) in results" :key="i" class="dot" @click="change(i+1)"></div>
-  </div>
+    </transition>
   </div>
 </template>
 
@@ -30,6 +56,7 @@ export default {
       showPointer: true,
       clickText: "Click the circle <br /> to continue",
       index: 0,
+      showModal: false,
       results: [
         {
           id: "1",
@@ -46,19 +73,19 @@ export default {
           left: 50,
         },
         {
-          id:'3',
+          id: "3",
           duration: 3,
-          content:'Thired content with duration 3',
-          top:10,
-          left:10,
+          content: "Thired content with duration 3",
+          top: 10,
+          left: 10,
         },
         {
-          id:'4',
+          id: "4",
           duration: 3,
-          content:'Fourth content with duration 3',
-          top:20,
-          left:20,
-        }
+          content: "Fourth content with duration 3",
+          top: 20,
+          left: 20,
+        },
       ],
     };
   },
@@ -67,12 +94,12 @@ export default {
       var player = document.getElementById(this.playerID);
       this.showPointer = false;
       this.index++;
-      
+
       if (!this.results[this.index]) {
         player.play();
-        setTimeout(()=>{
+        setTimeout(() => {
           // player.pause();
-        }, this.results[this.index - 1].duration * 1000 )
+        }, this.results[this.index - 1].duration * 1000);
         return;
       }
       console.log(this.index);
@@ -93,7 +120,9 @@ export default {
         console.log(instruction);
 
         this.content = instruction.content;
-
+        if(this.showModal == false){
+          this.ready();
+        }
         // if (this.results[this.index + 1]) {
         //   this.index++;
         // }else{
@@ -101,42 +130,52 @@ export default {
         // }
       }, instruction.duration * 1000);
     },
-    
+
+    restart() {
+      this.showModal = false;
+      var player = document.getElementById(this.playerID);
+      player.pause();
+      player.currentTime = 0;
+      this.index = 0;
+      this.ready();
+    },
+
+    ready() {
+      let instruction = this.results[this.index];
+
+      var pointer = document.getElementById(this.pointerID);
+      pointer.style.top = instruction.top + "%";
+      pointer.style.left = instruction.left + "%";
+      pointer.style.transform = ` translate(-${instruction.top}%, -${instruction.left}%) translate3d(0px, 0px, 0px) scale(1.6, 1.6)`;
+      this.content = instruction.content;
+      console.log("Position");
+      console.log(instruction);
+      console.log(this.index);
+    },
   },
   mounted() {
     // window.onresize = () => {
     //   console.log(window.innerWidth)
     // }
 
-    let instruction = this.results[this.index];
-
-    var pointer = document.getElementById(this.pointerID);
-    pointer.style.top = instruction.top + "%";
-    pointer.style.left = instruction.left + "%";
-    pointer.style.transform = ` translate(-${instruction.top}%, -${instruction.left}%) translate3d(0px, 0px, 0px) scale(1.6, 1.6)`;
-    this.content = instruction.content;
-    console.log("Position");
-    console.log(instruction);
-    console.log(this.index);
+    this.ready();
     // this.index++;
+
+    // Video information
+
+    var myVideoPlayer = document.getElementById(this.playerID);
+    // meta = document.getElementById('meta');
+
+    myVideoPlayer.addEventListener("loadedmetadata", function () {
+      var duration = myVideoPlayer.duration;
+      // console.log(myVideoPlayer);
+      console.log("Duration is " + duration.toFixed(2) + " seconds.");
+    });
   },
 };
 </script>
 
 <style scoped>
-.dots{
-  padding: 10px 0px;
-  display: flex;
-  justify-content: center;
-  gap:10px;
-}
-.dot{
-  background: red;
-  height: 15px;
-  width:15px;
-  border-radius: 100%;
-  cursor: pointer;
-}
 .djpp {
   display: flex;
   align-items: center;
@@ -271,5 +310,71 @@ export default {
 }
 @media (min-width: 1281px) {
   /* hi-res laptops and desktops */
+}
+
+/* Modal */
+
+.modal-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: table;
+  transition: opacity 0.3s ease;
+}
+
+.modal-wrapper {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.modal-container {
+  width: 90%;
+  margin: 0px auto;
+  padding: 20px 30px;
+  background-color: #fff;
+  border-radius: 2px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+  transition: all 0.3s ease;
+  font-family: Helvetica, Arial, sans-serif;
+}
+
+.modal-header h3 {
+  margin-top: 0;
+  color: #42b983;
+}
+
+.modal-body {
+  margin: 20px 0;
+}
+
+.modal-default-button {
+  float: right;
+}
+
+/*
+ * The following styles are auto-applied to elements with
+ * transition="modal" when their visibility is toggled
+ * by Vue.js.
+ *
+ * You can easily play with the modal transition by editing
+ * these styles.
+ */
+
+.modal-enter {
+  opacity: 0;
+}
+
+.modal-leave-active {
+  opacity: 0;
+}
+
+.modal-enter .modal-container,
+.modal-leave-active .modal-container {
+  -webkit-transform: scale(1.1);
+  transform: scale(1.1);
 }
 </style>
